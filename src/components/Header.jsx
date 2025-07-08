01d23ae2c9d2ef683eb7cc99988e53ec5702ecdc
kudaT <timilehinkudaisi01@gmail.com>
2025-07-08 05:47:57 -0700

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <>
+      <section>
+        <div><h2>gree</h2></div>
+      </section>
+    </>
+  );
+};
+
+export default Header;
